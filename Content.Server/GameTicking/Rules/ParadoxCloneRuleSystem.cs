86c3314fbe89384b19965ace4934af4604107e94
Princess Cheeseballs <66055347+Princess-Cheeseballs@users.noreply.github.com>
2026-05-12 13:23:54 -0700

    Ghost Roles Test (#43774)
    * dasfd
    
    * ghost role test finished
    
    * erm? heisentest?
    
    * shorter test name actually
    
    * actually even better
    
    * cleanup
    
    * better assertions
    
    * typo
    
    * linqless
    
    * burger
    
    * should work
    
    * oop
    
    * quadruple check for nullspace
    
    * bruh
    
    * grrr
    
    * try this???
    
    * reduce tolerance so long as it's close enough
    
    * better descriptionpush
    
    * A
    
    * hammed burger
    
    * uuh does that fix it?
    
    * try that?
    
    * review
    
    ---------
    
    Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>

--- Content.Server/GameTicking/Rules/ParadoxCloneRuleSystem.cs
+++ Content.Server/GameTicking/Rules/ParadoxCloneRuleSystem.cs
@@ -13,7 +13,6 @@ using Content.Shared.Gibbing.Components;
 using Content.Shared.Medical.SuitSensor;
 using Content.Shared.Mind;
 using Content.Shared.Objectives.Systems;
-using Content.Shared.Random.Helpers;
 using Robust.Shared.Random;
 
 namespace Content.Server.GameTicking.Rules;
@@ -25,7 +24,6 @@ public sealed partial class ParadoxCloneRuleSystem : GameRuleSystem<ParadoxClone
     // </Trauma>
     [Dependency] private IRobustRandom _random = default!;
     [Dependency] private CloningSystem _cloning = default!;
-    [Dependency] private SharedTransformSystem _transform = default!;
     [Dependency] private SharedMindSystem _mind = default!;
     [Dependency] private SuitSensorSystem _sensor = default!;
     [Dependency] private TargetSystem _target = default!;
@@ -56,9 +54,6 @@ public sealed partial class ParadoxCloneRuleSystem : GameRuleSystem<ParadoxClone
     // we have to do the spawning here so we can transfer the mind to the correct entity and can assign the objectives correctly
     private void OnAntagSelectEntity(Entity<ParadoxCloneRuleComponent> ent, ref AntagSelectEntityEvent args)
     {
-        if (args.Session?.AttachedEntity is not { } spawner)
-            return;
-
         if (ent.Comp.OriginalBody != null) // target was overridden, for example by admin antag control
         {
             if (Deleted(ent.Comp.OriginalBody.Value) || !_mind.TryGetMind(ent.Comp.OriginalBody.Value, out var originalMindId, out var _))
@@ -88,7 +83,7 @@ public sealed partial class ParadoxCloneRuleSystem : GameRuleSystem<ParadoxClone
 
         }
 
-        if (ent.Comp.OriginalBody == null || !_cloning.TryCloning(ent.Comp.OriginalBody.Value, _transform.GetMapCoordinates(spawner), ent.Comp.Settings, out var clone))
+        if (ent.Comp.OriginalBody == null || !_cloning.TryCloning(ent.Comp.OriginalBody.Value, args.Coords, ent.Comp.Settings, out var clone))
         {
             Log.Error($"Unable to make a paradox clone of entity {ToPrettyString(ent.Comp.OriginalBody)}");
             return;
